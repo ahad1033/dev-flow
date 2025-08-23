7a7cf9e25976d85451cd9c77a1b54a92f424a06d
ahad1033 <a.ahad2232@gmail.com>
2025-08-23 18:24:30 +0600

    Implemented db connection by mongoose and created account, answer, collection, interaction, question, tag, user and vote models

---
+++ database/index.ts
@@ -0,0 +1,21 @@
+import Tag from "./tag.model";
+import User from "./user.model";
+import Vote from "./vote.model";
+import Answer from "./answer.model";
+import Account from "./account.model";
+import Question from "./question.model";
+import Collection from "./collection.model";
+import Interaction from "./interaction.model";
+import TagQuestion from "./tag-question.model";
+
+export {
+  Tag,
+  User,
+  Vote,
+  Answer,
+  Account,
+  Question,
+  Collection,
+  Interaction,
+  TagQuestion,
+};
