e2974c7e8db30924cb4851383f1e354f0befe1f2
ahad1033 <a.ahad2232@gmail.com>
2025-08-23 09:09:56 +0600

    Enhance HomePage layout with search functionality, add new route for asking questions, and update package dependencies.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,22 +1,44 @@
-import { signOut } from "@/auth";
+import Link from "next/link";
+// import { signOut } from "@/auth";
 import { Button } from "@/components/ui/button";
 
 import ROUTES from "@/constants/route";
+import LocalSearch from "@/components/search/LocalSearch";
 
-const HomePage = () => {
+interface SearchParams {
+  searchParams: Promise<{ [key: string]: string }>;
+}
+
+const HomePage = async ({ searchParams }: SearchParams) => {
+  const { query = "" } = await searchParams;
+
+  // const filteredQuestions = questions.filter((question) => question.includes(query?.toLowerCase()))
   return (
-    <div>
-      <h1 className="font-bold">Welcome to DevFlow</h1>
+    <>
+      <section className="flex w-full flex-col-reverse sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Question</h1>
 
-      <form
-        action={async () => {
-          "use server";
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button className="px-10 pt-[100px]">Log out</Button>
-      </form>
-    </div>
+        <Button
+          asChild
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a question</Link>
+        </Button>
+      </section>
+
+      <section className="mt-11">
+        <LocalSearch
+          route="/"
+          otherClasses="flex-1"
+          imgSrc="/icons/search.svg"
+          placeholder="Search questions..."
+        />
+      </section>
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        <h1>question card</h1>
+      </div>
+    </>
   );
 };
 
