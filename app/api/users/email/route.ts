f31cfd8088d490037d59fa56a8d273516829008b
ahad1033 <a.ahad2232@gmail.com>
2025-08-24 01:42:00 +0600

    Add AccountSchema for user account validation with enhanced password requirements and created account, user routes.

---
+++ app/api/users/email/route.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+
+import dbConnect from "@/lib/mongoose";
+import handleError from "@/lib/handlers/error";
+
+import { User } from "@/database";
+import { UserSchema } from "@/lib/validation";
+import { APIErrorResponse } from "@/types/global";
+import { NotFoundError, ValidationError } from "@/lib/http-error";
+
+export async function POST(request: Request) {
+  const { email } = await request.json();
+
+  try {
+    await dbConnect();
+
+    const validatedData = UserSchema.partial().safeParse({ email });
+
+    if (!validatedData.success)
+      throw new ValidationError(validatedData.error.flatten().fieldErrors);
+
+    const user = await User.findOne({ email });
+    if (!user) throw new NotFoundError("User");
+
+    return NextResponse.json(
+      {
+        success: true,
+        data: user,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
